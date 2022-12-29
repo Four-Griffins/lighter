349a9d2917fe56a5fd3665a79116be2dd2d13760
Four_Griffins <four.griffins.dev@gmail.com>
2022-12-29 19:14:27 +0100

    moar uniforms = moar good

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ use notify::{RecommendedWatcher, RecursiveMode, Watcher, Config};
 
 #[derive(Parser)]
 struct Args {
-    ///redraw every frame. Use this for animations
+    ///Redraw every frame. Use this for animations
     #[arg(short, long, default_value_t = false)]
     live: bool,
 
-    ///path to a GLSL fragment shader to apply
+    ///Path to a GLSL fragment shader to apply
     #[arg()]
     path: String,
 }
@@ -23,6 +23,7 @@ implement_vertex!(Vertex, position);
 fn get_shader(display: &glium::Display, path: &str) -> glium::Program {
     let vert = include_str!("./vert.glsl");
     let frag = std::fs::read_to_string(path).expect("failed to read file");
+    //TODO better handling here to not panic
     glium::Program::from_source(display, vert, &frag, None).expect("compilation failed")
 }
 
@@ -31,10 +32,13 @@ fn draw(
     vertices: &glium::VertexBuffer<Vertex>,
     indices: &glium::IndexBuffer<u8>,
     shader: &glium::Program,
+    time: f32, frame: i32,
 ) {
     let size = display.get_framebuffer_dimensions();
     let uniforms = uniform! {
         u_resolution: [size.0 as f32, size.1 as f32, size.1 as f32 / size.0 as f32],
+        u_time: time,
+        u_frame: frame,
     };
     let mut target = display.draw();
     target.clear_color(0., 0., 0., 0.);
@@ -62,7 +66,7 @@ fn main() {
     let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList, &[0u8, 1, 2, 1, 2, 3]).unwrap();
     
     //draw once to make the window appear
-    draw(&display, &vertices, &indices, &shader);
+    draw(&display, &vertices, &indices, &shader, 0., 0);
     
     //watching changes in the user file, and sending a custom event to the glium event loop further down
     let (tx, rx) = std::sync::mpsc::channel();
@@ -82,26 +86,29 @@ fn main() {
         }
     });
     
+    let start_time = std::time::Instant::now();
+    let mut frame = 0;
     eventloop.run(move |ev, _, flow| {
         *flow = if args.live { ControlFlow::Poll } else { ControlFlow::Wait };
         
+        let time = (std::time::Instant::now() - start_time).as_secs_f32();
         match ev {
             Event::RedrawRequested(..) => {
-                if !args.live { draw(&display, &vertices, &indices, &shader); }
+                if !args.live { frame += 1; draw(&display, &vertices, &indices, &shader, time, frame); }
             }
             Event::WindowEvent { event, .. } => match event {
                 WindowEvent::CloseRequested => *flow = ControlFlow::Exit,
                 WindowEvent::Resized(..) => {
-                    if !args.live { draw(&display, &vertices, &indices, &shader); }
+                    if !args.live { frame += 1; draw(&display, &vertices, &indices, &shader, time, frame); }
                 }
                 _ => {},
             }
             Event::UserEvent(..) => {
                 shader = get_shader(&display, &args.path);
-                if !args.live { draw(&display, &vertices, &indices, &shader); }
+                if !args.live { frame += 1; draw(&display, &vertices, &indices, &shader, time, frame); }
             }
             Event::MainEventsCleared => {
-                if args.live { draw(&display, &vertices, &indices, &shader); }
+                if args.live { frame += 1; draw(&display, &vertices, &indices, &shader, time, frame); }
             }
             _ => {},
         }
